51af42df229a7d849c57c11963895912c368bde2
arjunsk <admin@arjunsk.com>
2023-01-13 20:54:53 -0600

    CN Adapter for DN Stl Vector

---
+++ pkg/vm/engine/tae/stl/containers/cn_vector_adapter.go
@@ -0,0 +1,165 @@
+package containers
+
+import (
+	"github.com/matrixorigin/matrixone/pkg/common/mpool"
+	cnVector "github.com/matrixorigin/matrixone/pkg/container/vector"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/common"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/containers"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/stl"
+	"io"
+	"unsafe"
+)
+
+type CnStlVector[T any] struct {
+	downstreamVector *cnVector.Vector
+}
+
+func NewStlVector[T any](opts ...containers.Options) *CnStlVector[T] {
+	vec := &CnStlVector[T]{}
+	return vec
+}
+
+func (vec CnStlVector[T]) Close() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Clone(offset, length int, allocator ...*mpool.MPool) stl.Vector[T] {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) ReadBytes(data *stl.Bytes, share bool) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Reset() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) IsView() bool {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Bytes() *stl.Bytes {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) WindowAsBytes(offset, length int) *stl.Bytes {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Data() []byte {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) DataWindow(offset, length int) []byte {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Slice() []T {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) SlicePtr() unsafe.Pointer {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) SliceWindow(offset, length int) []T {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Get(i int) (v T) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Append(v T) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) AppendMany(vals ...T) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Update(i int, v T) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Delete(i int) (deleted T) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) BatchDelete(rowGen common.RowGen, cnt int) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) BatchDeleteInts(sels ...int) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) BatchDeleteUint32s(sels ...uint32) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) GetAllocator() *mpool.MPool {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Capacity() int {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Length() int {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Allocated() int {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) String() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) Desc() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) WriteTo(writer io.Writer) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) ReadFrom(reader io.Reader) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (vec CnStlVector[T]) InitFromSharedBuf(buf []byte) (int64, error) {
+	//TODO implement me
+	panic("implement me")
+}
--- pkg/vm/engine/tae/stl/containers/vector.go
+++ pkg/vm/engine/tae/stl/containers/vector.go
@@ -15,14 +15,19 @@
 package containers
 
 func NewVector[T any](opts ...Options) *Vector[T] {
-	var v T
-	_, ok := any(v).([]byte)
-	if !ok {
-		return &Vector[T]{
-			Vector: NewStdVector[T](opts...),
-		}
-	}
+
 	return &Vector[T]{
-		Vector: NewStrVector[T](opts...),
+		Vector: NewStlVector[T](opts...),
 	}
+
+	//var v T
+	//_, ok := any(v).([]byte)
+	//if !ok {
+	//	return &Vector[T]{
+	//		Vector: NewStdVector[T](opts...),
+	//	}
+	//}
+	//return &Vector[T]{
+	//	Vector: NewStrVector[T](opts...),
+	//}
 }
